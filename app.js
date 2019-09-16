4287a82219542bf1540043150af5c143884bd243
Andrey Senko <avsenko@gmail.com>
2019-09-16 11:52:48 +0300

    Добавлена проверка jwt-токена, добавлены зависимости

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
-const {ApolloServer} = require("apollo-server");
+const {ApolloServer, AuthenticationError} = require("apollo-server");
 const {typeDefs} = require("./src/schema");
 const {resolvers} = require("./src/resolvers");
 const {serverOpts} = require("./src/config");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: ({req}) => {
+
+        const token = (req.headers.authorization || '').split(" ")[1];
+
+        if (undefined === token) {
+
+            throw new Error('Token is required');
+        }
+
+        const uid = jwt.verify(
+            token,
+            process.env.JWT_SECRET_KEY,
+            function (err, decoded) {
+
+                if (null !== err) {
+
+                    return null;
+                }
+
+                if (undefined === decoded || undefined === decoded.uid) {
+
+                    return null;
+                }
+
+                return decoded.uid;
+            }
+        );
+
+        if (null === uid) {
+
+            throw new AuthenticationError("Unauthorized");
+        }
+
+        console.log(uid);
+    }
 }).listen(
     serverOpts,
 ).then(({url}) => {
